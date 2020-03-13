1d47ef709a274a5161e16444ecdebfbc651ad3d9
Felipe Guimaraes <guima.felipec@gmail.com>
2020-03-13 15:13:58 -0300

    little fix

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const TOKEN = process.env.TOKEN;
 
 const options = {
 	webHook: {
-		port: process.env.PORT;
+		port: process.env.PORT
 	}
 }
 
 const url = process.env.APP_URL || 'https://panquecah-bot.herokuapp.com:433';
 
-//const bot = new TelegramBot(TOKEN, {polling: true});
-const bot = new TelegramBot(TOKEN, options);
+const bot = new TelegramBot(TOKEN, {polling: true});
+//const bot = new TelegramBot(TOKEN, options);
 
 const handlers = require('./message_handlers/handlers');
 const error_handlers = require('./error_handlers/handlers');

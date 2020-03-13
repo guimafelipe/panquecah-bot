9867836e28629422fb6891361f119e8525ae2a71
Felipe Guimaraes <guima.felipec@gmail.com>
2020-03-13 15:25:46 -0300

    adding webhook

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const url = process.env.APP_URL || 'https://panquecah-bot.herokuapp.com:433';
 //const bot = new TelegramBot(TOKEN, {polling: true});
 const bot = new TelegramBot(TOKEN, options);
 
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
 const handlers = require('./message_handlers/handlers');
 const error_handlers = require('./error_handlers/handlers');
 

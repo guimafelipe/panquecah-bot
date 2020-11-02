0dcda1b450a367fe45e392eefbc6587ba74a39c6
Felipe Guimaraes <guima.felipec@gmail.com>
2020-11-02 04:25:26 -0300

    limpando codigo do app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const TelegramBot = require('node-telegram-bot-api'),
-	  dotenv = require('dotenv'),
-	  mongoose = require('mongoose');
+const	TelegramBot = require('node-telegram-bot-api'),
+		dotenv = require('dotenv'),
+		mongoose = require('mongoose');
 
 dotenv.config();
 
@@ -10,7 +10,7 @@ const options = {
 	webHook: {
 		port: PORT || 443
 	}
-}
+};
 
 const url = APP_URL || 'https://panquecah-bot.herokuapp.com:443';
 
@@ -31,12 +31,11 @@ const 	handlers = require('./message_handlers/handlers'),
 		commands = require('./commands/commands'),
 		hp_handlers = require('./hp_handlers/handlers');
 
-mongoose.connect(DB_URI ||
-				'mongodb://localhost:27017/panquecahbot',
-				 {useNewUrlParser: true}).catch(error => {
+mongoose.connect(DB_URI || 'mongodb://localhost:27017/panquecahbot',
+				{useNewUrlParser: true}).catch(error => {
 					console.log("erro ao conectar no db");
 					console.log(error);
-				 });
+				});
 
 handlers.set_bot(bot);
 hp_handlers.set_bot(bot);

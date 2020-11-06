042156530dcda771c02fea9dba93c43c471b83f6
Felipe Guimaraes <guima.felipec@gmail.com>
2020-11-06 20:46:18 -0300

    actually printing the polling error

--- error_handlers/handlers.js
+++ error_handlers/handlers.js
@@ -18,4 +18,12 @@ handlers.init = function(){
 			console.log(e[property]);
 		}	
 	});
+
+	bot.on('polling_error', (e) => {
+		console.log('Polling error:');
+		console.log(e);
+		for(let property in e){
+			console.log(e[property]);
+		}	
+	});
 };
